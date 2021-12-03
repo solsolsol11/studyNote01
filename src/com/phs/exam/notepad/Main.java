bdfc865dcf3e058f3777213d38818accee1025b8
solsolsol11 <ushio941@gamail.com>
2021-12-03 11:26:56 +0900

    /usr/article/detail 추가

--- src/com/phs/exam/notepad/Main.java
+++ src/com/phs/exam/notepad/Main.java
@@ -1,5 +1,8 @@
 package com.phs.exam.notepad;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -11,13 +14,25 @@ public class Main {
         System.out.println("==프로그램 시작==");
 
         int articlesLastId = 0;
+        List<Article> articles = new ArrayList<>();
         while (true){
             System.out.printf("명령 ) ");
             String cmd = sc.nextLine();
 
             if(cmd.equals("exit")){
                 break;
-            }else if(cmd.equals("/usr/article/write")){
+            }else if(cmd.equals("/usr/article/detail")){
+                if(articles.isEmpty()){
+                    System.out.println("게시물이 없습니다");
+                    continue;
+                }
+                Article article = articles.get(articles.size() -1);
+                System.out.println("- 게시물 상세내용 -");
+                System.out.printf("번호 : %d\n",article.id);
+                System.out.printf("제목 : %s\n",article.title);
+                System.out.printf("내용 : %s\n",article.body);
+            }
+            else if(cmd.equals("/usr/article/write")){
 
 
                 System.out.println("게시물 등록");
@@ -29,8 +44,9 @@ public class Main {
                 int id = articlesLastId +1;
                 articlesLastId = id;
 
-                System.out.printf("%d번 게시물이 입력되었습니다.", id);
+                System.out.printf("%d번 게시물이 입력되었습니다.\n", id);
                 Article article = new Article(id,title,body);
+                articles.add(article);
                 System.out.println("입력된 객체" + article);
             }
 
