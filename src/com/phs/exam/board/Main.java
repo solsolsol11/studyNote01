33f5a1a6b5604d5e43484d7423c11e6b95dd93e9
solsolsol11 <ushio941@gmail.com>
2021-12-03 20:36:12 +0900

    테스트1

--- src/com/phs/exam/board/Main.java
+++ src/com/phs/exam/board/Main.java
@@ -1,78 +1,96 @@
 package com.phs.exam.board;
+import java.util.*;
 
-import java.util.Scanner;
-
-/*
-[ ]환영메세지 출력
-[ ]고객으로부터 명령어 입력받기
-  [ ]스캐너 객체라도 하나 만들어보기
-  [ ]받은 명령어 출력하기
-[ ]exit 명령어 처리
-  [ ]exit 입력받으면 종료 라고 출력하기
-  [ ]exit 입력받을 때 까지 계속 실행
-[ ]/usr/article/write 명령어 입력처리
-  [ ]시작문구라도 출력
-  [ ]제목과 내용이라도 입력받기
-  [ ]생성된 게시물 번호 출력
-  [ ]생성될 때 마다 게시물번호가 증가
-  [ ]생성된 게시물을 객체에 담기
-  [ ]Article 객체 생성하기
-  [ ]Article 클래스 생성하기
-[ ]/usr/article/detail 명령어 입력처리
-[ ]명령어를 제대로 입력했다면, 입력받은 명령어 문구는 안나오도록
-[ ]toString 메서드 오버라이드 하기
-[ ]생성자 도입
-*/
 public class Main {
 
+  private static void makeTestcase(List<Article> articles) {
+    for(int i = 0; i < 100; i++){
+      int id= i + 1;
+      articles.add(new Article(id,"제목" + id,"내용" + id));
+    }
+  }
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println("==게시판 v 0.1 ==");
-    System.out.println("== 프로그램 시작 ==");
-    int articleLastId = 0;
+    System.out.println("- 게시판 v 0.1 -");
+    System.out.println("==프로그램 시작==");
+    int articlesLastId = 0;
+    List<Article> articles = new ArrayList<>();
+    makeTestcase(articles);
     while (true){
       System.out.printf("명령 ) ");
       String cmd = sc.nextLine();
       if(cmd.equals("exit")){
-      break;
-      }else if(cmd.equals("/usr/article/write")){
-        System.out.println("- 게시물 등록 - ");
+        break;
+      }else if(cmd.equals("/usr/article/list")){
+        System.out.println("- 게시물 리스트 -");
+        System.out.println("-------------------");
+        System.out.println("번호 / 제목");
+        System.out.println("-------------------");
+        for(int i = articles.size() -1 ; i >= 0; i--){
+          Article article = articles.get(i);
+          System.out.printf("%d / %s\n",article.id,article.title);
+        }
+      }
+      else if(cmd.equals("/usr/article/detail")){
+        if(articles.isEmpty()){
+          System.out.println("게시물이 없습니다");
+          continue;
+        }
+        Article article = articles.get(articles.size() -1);
+        System.out.println("- 게시물 상세내용 -");
+        System.out.printf("번호 : %d\n",article.id);
+        System.out.printf("제목 : %s\n",article.title);
+        System.out.printf("내용 : %s\n",article.body);
+      }
+      else if(cmd.equals("/usr/article/write")){
+        System.out.println("게시물 등록");
         System.out.printf("제목 : ");
         String title = sc.nextLine();
-        System.out.printf("내용 : ");
+        System.out.printf("제목 : ");
         String body = sc.nextLine();
-
-        int id = articleLastId + 1;
-        articleLastId = id;
-        Article article = new Article();
-        article.id = id;
-        article.title = title;
-        article.body = body;
-        System.out.println("생성된 게시물 객체 :" + article);
-        System.out.printf("%d번 게시물이 입력되었습니다\n",id);
+        int id = articlesLastId +1;
+        articlesLastId = id;
+        System.out.printf("%d번 게시물이 입력되었습니다.\n", id);
+        Article article = new Article(id,title,body);
+        articles.add(article);
+        System.out.println("입력된 객체" + article);
       }
-
-
     }
-    System.out.println("== 프로그램 종료 ==");
     sc.close();
-
-
-
+    System.out.println("==프로그램 종료==");
   }
 }
-
 class Article{
   int id;
   String title;
   String body;
+  Article(int id,String title,String body){
+    this.id = id;
+    this.title = title;
+    this.body = body;
+  }
   @Override
   public String toString(){
-    return String.format("{id : \"%d\" title : \"%s\" body : \"%s\"}",id,title,body);
+    return String.format("{id : %d title : %s body : %s}",id,title,body);
+  }
+
+}
+
+class Rq{
+  String url;
+
+  Rq(String url){
+    this.url = url;
+  }
+
+  static Map<String, String> getParams(){
+    return null;
+  }
+  static String getUrlPath(){
+    return null;
   }
 }
 
+class Util{
 
-
-
-
+}
